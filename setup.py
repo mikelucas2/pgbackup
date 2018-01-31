dd57fb230a55edc21a29eafa30470a4cf1b867e9
Mike Lucas <mike.lucas1@telus.com>
2018-01-31 23:11:07 +0000

    Initial implementation of CLI

--- setup.py
+++ setup.py
@@ -12,5 +12,8 @@ setup(
     author_email='mike.lucas1@telus.com',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    install_requires=[]
+    install_requires=['boto3'],
+    entry_points={
+        'console_scripts': ['pgbackup=pgbackup.cli:main'],
+    },
 )
--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -15,7 +15,7 @@ def create_parser():
     Back up PostgreSQL databases locally or to AWS S3
     """)
     parser.add_argument('url', help='URL of database to backup')
-    parser.add_argument('--driver',
+    parser.add_argument('--driver', '-d',
             help='how & where to store backup',
             nargs=2,
             metavar=("DRIVER", "DESTINATION"),
@@ -23,3 +23,22 @@ def create_parser():
             required=True
             )
     return parser
+
+def main():
+    import time
+    import boto3
+    from pgbackup import pgdump, storage
+
+    args = create_parser().parse_args()
+    dump = pgdump.dump(args.url)
+    if args.driver == 's3':
+        client = boto3.client('s3')
+        timestamp = time.strftime("%Y-%m-%dT%H:%M", time.localtime())
+        file_name = pgdump.dump_file_name(args.url, timestamp)
+        print("Backing database up to %s in S3 as %s" % (args.destination, file_name))
+        storage.s3(client, dump.stdout, args.destination, file_name)
+    else:
+        outfile = open(args.destination, 'w')
+        print("Backing database up locally to %s" % outfile.name)
+        storage.local(dump.stdout, outfile)
+
